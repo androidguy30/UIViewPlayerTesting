13f471d2d3ebd3834f708bca4f3fe1201ac34487
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2023-11-01 16:05:35 +0530

    Initial Commit

---
+++ UIViewPlayerTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIViewPlayerTesting
+//
+//  Created by Kausthubh adhikari on 01/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
